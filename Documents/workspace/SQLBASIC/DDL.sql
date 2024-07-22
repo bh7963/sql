a751fbc0a647f209ca71d36a1b380ecdd0bdbfac
bh7963 <ajtwoddl7607@gmail.com>
2024-07-22 15:28:22 +0900

    2024.07.22 12:50

--- Documents/workspace/SQLBASIC/DDL.sql
+++ Documents/workspace/SQLBASIC/DDL.sql
@@ -16,10 +16,50 @@ example_column1 INT,
 example_column2 BOOLEAN
 );
 -- 컬럼 데이터 타입
-
+CREATE TABLE data_table (
+	-- INT : 정수 타입
+    int_column INT,
+    -- DOUBLE, FLOAT : 실수 타입
+    double_column DOUBLE,
+    float_column FLOAT,
+    -- BOOLEAN : 논리 타입
+	boolean_column BOOLEAN,
+    -- VARCHAR(문자열길이) : 가변길이 문자열
+    string_column VARCHAR(10),
+    -- TEXT : 단순 장문 문자열
+    text_column TEXT,
+    -- DATE : 날짜
+    date_column DATE,
+    -- DATETIME : 날짜 및 시간
+    datetime_column DATETIME
+);
 
 -- 사용자 생성
 -- CREATE USER '사용자명'@'접속IP' IDENTIFIED BY '비밀번호'
 CREATE USER 'developer'@'127.0.0.1' IDENTIFIED BY '1234';
 CREATE USER 'developer'@'%' IDENTIFIED BY '1234';
 
+-- DROP : 데이터 구조(스키마)를 삭제하는 명령어
+-- DROP 스키마명
+
+-- 사용자 삭제
+DROP USER 'developer'@'%';
+
+-- 테이블 삭제
+-- g해당 테이블을 참조하고 있는 다른 테이블이 존재하면 테이블 삭제가 불가능
+DROP TABLE example_table;
+
+-- 데이터베이스 삭제	
+DROP DATABASE practice_sql;
+
+-- ALTER : 구조를 변경하는 명령어 -> 앵간하면 하지 않는게 좋다(첨부터 설계잘해야 됨)
+-- 1. 테이블의 컬럼 추가
+ALTER TABLE example_table ADD example_column3 VARCHAR(10);
+-- 2. 테이블 컬럼 삭제
+ALTER TABLE example_table DROP COLUMN example_column3;
+-- 3. 테이블 컬럼 타입수정
+ALTER TABLE example_table MODIFY COLUMN example_column2 TEXT;
+-- 4. 테이블 컬럼 전체수정
+ALTER TABLE example_table CHANGE example_column1 column1 VARCHAR(20);
+-- 5. 데이터베이스 문자셋 수정
+ALTER DATABASE practice_sql DEFAULT CHARACTER SET utf8;
